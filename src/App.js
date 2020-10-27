5e52112352793199e36939813cac4d84f0d16a92
Otso Kurkela <otso.kurkela@outlook.com>
2020-10-27 16:48:53 +0200

    install React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import './App.css';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Sijainti from './components/sijainti/Sijainti';
+import Tehtavat from './components/tehtavat/Tehtavat';
+import Home from './components/Home';
+import Rastilistat from './components/rastilistat/Rastilistat';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Tässä on Klapi</h1>
-    </div>
+    <React.Fragment>
+
+      <Router>
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/sijainti" exact component={Sijainti} />
+            <Route path="/tehtavat" exact component={Tehtavat} />
+            <Route path="/rastilistat" exact component={Rastilistat} />
+          </Switch>
+        </div>
+      </Router>
+
+    </React.Fragment>
+
   );
 }
 
