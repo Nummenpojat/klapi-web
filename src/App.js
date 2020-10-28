b10aeaccba64d4e13fd7f98a7a7e8a57bb118f93
elias <elkkuulkku@gmail.com>
2020-10-28 14:47:18 +0200

    updated color, removed unneeded import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Nav from './components/Nav';
