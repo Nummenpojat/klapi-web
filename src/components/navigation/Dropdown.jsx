62325bbc55fc784fb3f2b659f4716f6f3d945c4b
Ode42 <ode.progr@outlook.com>
2021-01-01 18:37:49 +0200

    Actual profile and settings links

--- src/components/navigation/Dropdown.jsx
+++ src/components/navigation/Dropdown.jsx
@@ -1,13 +1,20 @@
 /* copyright (c) 2020- Otso Kurkela */
 
-import React from 'react';
+// import 3rd party components
+import { Link } from 'react-router-dom';
 
 export default () => {
     return(
         <div className="dropdown-invisible" id="dropdown">
-            <p>Dropdown item</p>
-            <p>Dropdown item</p>
-            <p>Dropdown item</p>
+            <div className="dropdown-links">
+            <Link to="/settings">
+                <p>Settings</p>
+            </Link>
+            <Link to="/profile">
+                <p>Profile</p>
+            </Link>
+            </div>
+            
         </div>
     );
 }
