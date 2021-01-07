cd4af43f505ca6a474ea5f482a44a775fa7eb88a
Ode42 <ode.progr@outlook.com>
2021-01-07 17:38:33 +0200

    Start conditional rendering of bottom links

--- src/components/Router/MenuComponents.tsx
+++ src/components/Router/MenuComponents.tsx
@@ -3,10 +3,16 @@ import BottomMenu from './../Navigation/menus/BottomMenu';
 
 // this component contains menus for app, they are displayed always
 export default (props:any) => {
+    const links = [
+        {
+            title: "Moiz",
+            link: "/sandfr"
+        }
+    ]
     return (
         <>
         <TopMenu />
-        <BottomMenu bottomLinks={props.bottomLinks} />
+        <BottomMenu scoutMode={props.scoutMode} />
         </>
     );
 }
